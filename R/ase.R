65ebba49052d31cd22f4dfabce2fe93ab75153ea
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2014-10-06 15:49:20 +0200

    Added payroll data

---
+++ R/ase.R
@@ -0,0 +1,18 @@
+#' SURVEY ON HOUSEHOLD INCOME AND WEALTH data
+#' 
+#' A dataset contains data about payroll workers from the SWIG
+#' 
+#' \itemize{
+#'   \item wage. numeric: wages for payroll workers
+#'   \item educ. numeric: years of education
+#'   \item educf. factor: education levels. 
+#'   \item gender. factor: gender 
+#'   \item age. numeric: age of respondent 
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name payroll
+#' @usage data(payroll)
+#' @format A data frame with 5700 rows and 5 variables
+NULL
