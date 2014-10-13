a1509175a06cccd7657a75dcf014c6d13b7149f7
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2014-10-14 00:12:10 +0200

    Added attend dataset

--- R/ase.R
+++ R/ase.R
@@ -1,6 +1,6 @@
 #' SURVEY ON HOUSEHOLD INCOME AND WEALTH data
 #' 
-#' A dataset contains data about payroll workers from the SWIG
+#' A dataset contains data about payroll workers from the SWIG (Bank of Italy).
 #' 
 #' \itemize{
 #'   \item wage. numeric: wages for payroll workers
@@ -15,4 +15,31 @@
 #' @name payroll
 #' @usage data(payroll)
 #' @format A data frame with 5700 rows and 5 variables
+NULL
+
+#' Data on test score and attendance.
+#' 
+#' These data were collected by Professors Ronald Fisher and Carl Liedholm
+#' during a term in which they both taught principles of microeconomics at
+#' Michigan State University.  
+#' 
+#' \itemize{
+#' \item  attend  classes attended out of 32
+#' \item  termGPA GPA for term
+#' \item  priGPA  cumulative GPA prior to term
+#' \item  ACT     ACT score
+#' \item  final   final exam score
+#' \item  atndrte percent classes attended
+#' \item  hwrte   percent homework turned in
+#' \item  frosh   =1 if freshman
+#' \item  soph    =1 if sophomore
+#' \item  missed  number of classes missed
+#' \item  stndfnl (final - mean)/sd
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name attend
+#' @usage data(attend)
+#' @format A data frame with 680 rows and 11 variables
 NULL
